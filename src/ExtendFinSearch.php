4c9d6e55ee433ef7adf6b0253f489247340ee34d
Dominik Brader <d.brader@findologic.com>
2020-10-20 11:23:22 +0200

    Remove autoloading and use shopware default services

--- src/ExtendFinSearch.php
+++ src/ExtendFinSearch.php
@@ -6,39 +6,26 @@ use FINDOLOGIC\FinSearch\FinSearch;
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Exception\PluginNotInstalledException;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class ExtendFinSearch extends Plugin
 {
-    public function build(ContainerBuilder $container): void
-    {
-        require_once $this->getBasePath() . '/vendor/autoload.php';
-        parent::build($container);
-
-        // Only load relevant classes if FinSearch is available
-        $activePlugins = $container->getParameter('kernel.active_plugins');
-        if (!isset($activePlugins[FinSearch::class])) {
-            return;
-        }
-
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/Resources/config'));
-        $loader->load('finsearch_services.xml');
-    }
-
-    /**
-     * @throws PluginNotInstalledException
-     */
     public function install(InstallContext $installContext): void
     {
         parent::install($installContext);
 
-        // Only install this plugin if FinSearch plugin is installed and active
-        $activePlugins = $this->container->getParameter('kernel.active_plugins');
-
-        if (!isset($activePlugins[FinSearch::class])) {
+        // Only install this plugin if FinSearch plugin is installed and active.
+        if (!$this->isMainPluginActive()) {
             throw new PluginNotInstalledException('FinSearch');
         }
     }
+
+    private function isMainPluginActive(): bool
+    {
+        $activePlugins = $this->container->getParameter('kernel.active_plugins');
+
+        return isset($activePlugins[FinSearch::class]);
+    }
 }
+
+// Uncomment this line if you require external dependencies for the extension plugin.
+//require_once $this->getBasePath() . '/vendor/autoload.php';
